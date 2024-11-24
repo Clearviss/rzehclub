859eecdf91b9877ee5921edf4ec39629d87556a6
Clearviss <domi500domi@gmail.com>
2024-11-24 02:05:19 +0100

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    let moving = false;
+
+    function isTransitioning(element: HTMLElement): boolean {
+        const computedStyle = getComputedStyle(element);
+        const transitionDuration = parseFloat(computedStyle.transitionDuration);
+        const transitionDelay = parseFloat(computedStyle.transitionDelay);
+        return transitionDuration + transitionDelay > 0;
+    }
+
+    function changePos(event: any, arg: boolean) {
+        const element = event.target as HTMLElement;
+        if (arg) {
+            element.style.transform = "translatey(-25px)";
+            element.style.transition =
+                "transform 0.3s ease, box-shadow 0.3s ease";
+
+            const onTransitionEnd = () => {
+                element.removeEventListener("transitionend", onTransitionEnd);
+            };
+            element.addEventListener("transitionend", onTransitionEnd);
+        }
+        if (!arg && !moving) {
+            element.style.transform = "translatey(0px)";
+            element.style.transition =
+                "transform 0.3s ease, box-shadow 0.3s ease";
+        }
+    }
+
+    function moveOut(event: any) {
+        moving = true;
+        const element = event.target as HTMLElement;
+
+        element.style.transition = "transform 0.5s ease";
+        element.style.transform = "translatey(-500px)";
+
+        const onTransitionEnd = () => {
+            element.removeEventListener("transitionend", onTransitionEnd);
+            element.remove();
+            goto("/home");
+        };
+
+        element.addEventListener("transitionend", onTransitionEnd);
+    }
+</script>
+
+<div class="h-screen flex justify-center items-center select-none">
+    <button
+        class="text-4xl p-6"
+        on:mouseenter={() => changePos(event, true)}
+        on:mouseleave={() => changePos(event, false)}
+        on:click={() => moveOut(event)}
+    >
+        Welcome
+    </button>
+</div>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        const texts = ["Loading", "Loading.", "Loading..", "Loading..."];
+        let i = 0;
+
+        const interval = setInterval(() => {
+            document.querySelector(".italic")!.textContent = texts[i];
+            i = (i + 1) % texts.length;
+        }, 500);
+
+        setTimeout(() => {
+            window.location.href = "/";
+        }, 5000);
+    });
+</script>
+
+<div class="h-screen flex flex-col gap-4 justify-center items-center">
+    <div class="loader"></div>
+    <div class="italic opacity-30 select-none">Loading</div>
+</div>
+
+<style>
+    /* HTML: <div class="loader"></div> */
+    .loader {
+        width: 120px;
+        height: 20px;
+        transform: skewX(-45deg);
+        background: linear-gradient(#000000 0 0) left -30px top 0/30px 20px no-repeat
+            #ccc;
+        animation: l3 1s infinite linear;
+    }
+
+    @keyframes l3 {
+        100% {
+            background-position: right -30px top 0;
+        }
+    }
+</style>
